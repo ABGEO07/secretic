9deca25f3dc66e6603ebc83af262d1b0ed0baee1
ABGEO <takalandzet@gmail.com>
2020-04-09 20:38:06 +0400

    :tada: Initial commit.

---
+++ src/main/java/dev/abgeo/secretic/SecreticApplication.java
@@ -0,0 +1,13 @@
+package dev.abgeo.secretic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecreticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecreticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/abgeo/secretic/SecreticApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abgeo.secretic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecreticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
